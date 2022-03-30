20faf9e724e59e11139f1003dcd7cce95c7b08f1
danduong <danduong@yahoo.com>
2022-03-30 17:56:42 +0800

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 public class Main {
 
-    public static double salaryCalculator(double hoursPerWeek,
-                                          double amountPerHour,
-                                          int vacationDays) {
-        if (hoursPerWeek < 0) {
-            return -1;
-        }
-
-        if (amountPerHour < 0) {
-            return -1;
-        }
-
-        double weeklyPaycheck = hoursPerWeek * amountPerHour;
-        double unpaidTime = vacationDays * amountPerHour * 8;
-        return (weeklyPaycheck * 52) - unpaidTime;
-    }
-
     public static void main(String[] args) {
-        double salary = salaryCalculator(40, 15, 8);
-        System.out.println(salary);
+        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
+
+        System.out.println("Triangle A area is " + triangleA.getArea());
+        System.out.println("Triangle B area is " + triangleB.getArea());
+        System.out.println("");
+
+        System.out.println("Using class instance method ");
+        System.out.println("Triangle A base is " + triangleA.getBase());
+        System.out.println("Triangle A side 1 is " + triangleA.getSide1());
+        System.out.println("");
+
+        System.out.println("Using class static method ");
+        System.out.println("Triangle A side 1 is " + Triangle.calculateArea(15, 8));
+        System.out.println("");
+
+        System.out.println("Using class static variable ");
+        System.out.println("Triangle has " + Triangle.nbOfSides() + " sides");
+        System.out.println("");
     }
 
 }
